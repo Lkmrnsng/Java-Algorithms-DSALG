36276b0b3628854a9fb9fff31c95ac68a1b55149
Megan Sioco <megan_rylene_sioco@dlsu.edu.ph>
2023-09-26 20:45:30 +0800

    tweaked runtime computation

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-		/*fr megan: I added in all the other txt files than random100000! for testing lang*/
 		//String filePath = "data/random100.txt";
 		//String filePath = "data/random25000.txt";
 		//String filePath = "data/random50000.txt";
@@ -11,22 +10,22 @@ public class Main {
         FileReader fileReader = new FileReader();
         SortingAlgorithms algos = new SortingAlgorithms();
         Record[] records = fileReader.readFile(filePath);
+		long startTime = System.currentTimeMillis();
 
-	long startTime = System.currentTimeMillis();
-        // YOUR ALGORITHM HERE
-        long endTime = System.currentTimeMillis(); 
-        long executionTime = endTime - startTime;
-
-	    
-        //algos.insertionSort(records, records.length);
+        algos.insertionSort(records, records.length);
         //algos.selectionSort(records, records.length);
-	//algos.mergeSort(records, 0, records.length - 1);
-	//algos.quickSort(records, 0, records.length - 1);
+		//algos.mergeSort(records, 0, records.length - 1);
+		//algos.quickSort(records, 0, records.length - 1);
 
+		long endTime = System.currentTimeMillis(); 
+        long executionTime = endTime - startTime;
+		
         for (Record record : records) {
             int id = record.getIdNumber();
             String name = record.getName();
             System.out.println(id + " " + name);
         }
+		
+		System.out.println("\nExecution Time = " + executionTime);
     }
 }
